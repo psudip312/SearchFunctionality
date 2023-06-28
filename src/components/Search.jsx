c4749e7e9957a13a00a3e2a3c8f7129575d91930
psudip312 <psudip312@gmail.com>
2023-06-28 16:00:32 +0545

    price range added

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -7,11 +7,13 @@ const Search = () => {
   const [searchResults, setSearchResults] = useState([]);
   const [baseurl, setBaseUrl] = useState('https://fakestoreapi.com/products');
   const [selectedCategory, setSelectedCategory] = useState('');
+  const [priceRange, setPriceRange] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         let url = baseurl;
+
         const response = await axios.get(url);
         setSearchResults(response.data);
       } catch (error) {
@@ -20,7 +22,7 @@ const Search = () => {
     };
 
     fetchData();
-  }, [searchQuery, selectedCategory]);
+  }, [searchQuery, selectedCategory, priceRange]);
 
   const handleSearchInputChange = (event) => {
     setSearchQuery(event.target.value);
@@ -32,7 +34,9 @@ const Search = () => {
     setBaseUrl(category ? `https://fakestoreapi.com/products/category/${category}` : 'https://fakestoreapi.com/products');
   };
 
- 
+  const handlePriceRangeChange = (event) => {
+    setPriceRange(event.target.value);
+  };
 
   return (
     <div className="search-container">
@@ -55,11 +59,23 @@ const Search = () => {
             <option value="women's clothing">Women's Clothing</option>
           </select>
         </div>
-    
+        <div className="price-range-filter">
+          <label htmlFor="price-range">Price Range:</label>
+          <select id="price-range" value={priceRange} onChange={handlePriceRangeChange}>
+            <option value="">All</option>
+            <option value="20">Less than $20</option>
+            <option value="50">Less than $50</option>
+            <option value="100">Less than $100</option>
+            <option value="200">Less than $200</option>
+            <option value="500">Less than $500</option>
+            <option value="700">Less than $700</option>
+            <option value="1000">Less than $1000</option>
+          </select>
+        </div>
       </div>
       <ul className="search-results">
         {searchResults
-          .filter((result) => result.title.includes(searchQuery))
+          .filter((result) => result.title.includes(searchQuery) && (!priceRange || result.price < priceRange))
           .map((result) => (
             <li key={result.id}>
               <h3>{result.title}</h3>
